fdb2506b0a9695708aba68abec11ebaf86001d3b
Steven Lee <koreancoffe@gmail.com>
2020-02-01 02:48:38 -0500

    added UwU to the scene

---
+++ UnityGGJ2020/Assets/Scripts/InventorySystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySystem : MonoBehaviour
+{
+    [SerializeField] private Item[] itemList = new Item[15];
+    [SerializeField] private Button[] itembuttoms = new Button[15];
+
+    [SerializeField] private Image uiWindow = null;
+
+
+    public void Remove(Item item)
+    {
+        itemList[(int)item.itemAsset] = null;
+    }
+
+    public void Add(Item item){
+            itemList[(int)item.itemAsset] = item;
+
+    } 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     /*   if (Input.GetKey("r")) {
+        } */
+    }
+}
+
+    public enum I { apple, banana, carrot, dog, elephant, fuck, gorilla, help, intense, james, kelome, lagrange, macdonald,
+    nomad, octopus, prakash };
---
+++ UnityGGJ2020/Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+[CreateAssetMenu(menuName = "Item")]
+public class Item : ScriptableObject
+{
+    public Sprite sprite;
+    public I itemAsset;
+}
+
