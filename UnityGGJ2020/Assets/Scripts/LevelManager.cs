437163c1d5eafbbee280824c8dec42608ad69cb4
Samantha Difeo <sdifeo1@gmail.com>
2020-02-01 09:34:12 -0500

    Scene changes and canvas fit to screen size

---
+++ UnityGGJ2020/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void GoToNextScene()
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
