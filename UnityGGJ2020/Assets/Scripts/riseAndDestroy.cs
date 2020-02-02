4075d8489593746f7b893cc54638291b6c7d6c4b
mr-fufu <monkeymanp8@gmail.com>
2020-02-02 15:02:53 -0500

    small changes plz dont merge conflict

---
+++ UnityGGJ2020/Assets/Scripts/riseAndDestroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class riseAndDestroy : MonoBehaviour
+{
+    private Vector3 hold;
+    public float life;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hold = gameObject.transform.position;
+        hold.y += 10f * Time.deltaTime;
+        gameObject.transform.position = hold;
+
+        life -= 1f;
+        if (life < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
