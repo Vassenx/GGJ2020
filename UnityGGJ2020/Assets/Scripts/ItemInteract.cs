d5b44934e7a5a5a961064ac444a9536f0e90579a
msukkari <michael.sukkarieh@mail.mcgill.ca>
2020-02-01 20:54:06 -0500

    implemented hover over text for items

--- UnityGGJ2020/Assets/Scripts/ItemInteract.cs
+++ UnityGGJ2020/Assets/Scripts/ItemInteract.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class ItemInteract : MonoBehaviour, Interactable
 {
+    Item item;
+
+    public void Start()
+    {
+        item = gameObject.GetComponent<Item>();
+    }
+
     public void HoverInteract()
     {
-
+        Debug.Log("HOVERING OVER ITEM");
     }
 
     public void ClickInteract()
     {
-
+        Debug.Log("CLICKING AN ITEM");
     }
 
     public void KeyInteract()
     {
 
+    }
+
+    public void OnMouseEnter()
+    {
+        item.descriptionBox.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        item.descriptionBox.SetActive(false);
     }
 }
