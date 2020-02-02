98be922a0bba13d45af2877410611bbe3cfc2f45
Steven Lee <koreancoffe@gmail.com>
2020-02-02 02:44:41 -0500

    Update DragItem.cs

--- UnityGGJ2020/Assets/Scripts/DragItem.cs
+++ UnityGGJ2020/Assets/Scripts/DragItem.cs
@@ -5,82 +5,42 @@ using UnityEngine.EventSystems;
 
 public class DragItem : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-    /*
+    private Vector3 lastPosition;
     [SerializeField]
-    private bool isDragable = false;
+    private GameObject triggerObject;
+    Collider itemCollider;
+    private Vector3 triggerPosition;
 
-    private GameObject toDrag;
-    private Vector2 offset;
+    //camera shake import
 
-    // Start is called before the first frame update
-    void Start(){ }
-
-    private void DragCommand() {
-        var inputPos = CurrentPos;
-        if (isDragable)
-        {
-            toDrag.transform.position = inputPos + offset;
-        }
-        else {
-            RaycastHit2D[] touches = Physics2D.RaycastAll(inputPos, inputPos, 0.5f);
-            if(touches.Length >0){
-                var hit = touches[0];
-                if(hit.transform != )
-                {
-                    isDragable = true;
-                    toDrag = hit.transform.gameObject;
-                    offset = (Vector2)hit.transform.position - inputPos;
-                    toDrag.transform.localScale = new Vector3(2f, 2f, 2f);
-                }
-            }
-        }
-    }
-
-    private void ResetPosition() {
-        isDragable = false;
-        toDrag.transform.localScale = new Vector3(1f,1f,1f);
-    }
-
-    Vector2 CurrentPos{
-        get
-        {
-            Vector2 pos;
-            pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            return pos;
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0)) {
-            DragCommand();
-        }
-        else {
-            if (isDragable) {
-                ResetPosition();
-            }
-
-        }
-    }
-    */
-    private Vector2 lastPosition;
 
     public void Start()
     {
         lastPosition = transform.position;
+        triggerPosition = triggerObject.transform.position;
+    }
+
+    public void Update()
+    {
+
+        if (transform.position == triggerPosition)
+        {
+            Debug.Log("It's hitting!");
+            Camera.main.GetComponent<CameraShake>().ShakeCamera();
+        }
     }
 
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = Input.mousePosition;
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         transform.localPosition = lastPosition;
     }
+
 }
 
 
