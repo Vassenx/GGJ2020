4f3ab0de6eaeca2b7342952da5f0eb5b5b019d44
mr-fufu <monkeymanp8@gmail.com>
2020-02-02 13:45:34 -0500

    mainSceneUpdated

--- UnityGGJ2020/Assets/Scripts/LadderPickup.cs
+++ UnityGGJ2020/Assets/Scripts/LadderPickup.cs
@@ -1,36 +1,26 @@
 ﻿using UnityEngine;
 
-public class LadderPickup : MonoBehaviour, Interactable
+public class LadderPickup : MonoBehaviour
 {
     // [SerializeField] private Item item;
     [SerializeField] private GameObject player;
-    [SerializeField] private GameObject exitObject;
-
-    public void HoverInteract()
-    {
-        //teleport the player up the stairs or start animation
-        //player.transform.position = ;
-
-    }
-
-    public void ClickInteract()
-    {
-        
-    }
+    public GameObject exitObject;
+    public bool open;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            KeyInteract();
-        }
-    }
-
-    public void KeyInteract()
-    {
-        if(Vector2.Distance(player.transform.position, transform.position) <= 5f)
-        {
-            player.transform.position = exitObject.transform.position;
+            if (open) {
+                if (Vector2.Distance(player.transform.position, transform.position) <= 20f)
+                {
+                    exitObject.SetActive(false);
+                    player.transform.position = exitObject.transform.position;
+                    exitObject.SetActive(true);
+                    exitObject.GetComponent<LadderPickup>().open = true;
+                    open = false;
+                }
+            }
         }
     }
 }
