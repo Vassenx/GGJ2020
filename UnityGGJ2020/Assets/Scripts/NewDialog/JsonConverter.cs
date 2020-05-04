d8c5cdccb060ec545bdc5c558504a5b7c118e3b7
Samantha Difeo <sdifeo1@gmail.com>
2020-05-03 23:28:55 -0400

    Add json convert script

---
+++ UnityGGJ2020/Assets/Scripts/NewDialog/JsonConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class JsonConverter
+{
+    public void LoadJson(string path)
+    {
+        using (StreamReader r = new StreamReader(path))
+        {
+            string rawJson = r.ReadToEnd();
+            DialogTree tree = JsonUtility.FromJson<DialogTree>(rawJson);
+            Debug.Log(tree.nodes[0].text);
+        }
+    }
+
+    [System.Serializable]
+    public struct DialogTree
+    {
+        public string[] conditions;
+        public int act;
+        public DialogNode[] nodes; 
+    }
+
+
+    [System.Serializable]
+    public struct DialogNode
+    {
+        public string speaker;
+        public string text;
+        public string[] choices;
+    }
+}
