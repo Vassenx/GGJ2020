a1d52d100bfe6a34273f747f760cc67292c95c8d
StevenPC <koreancoffe@gmail.com>
2020-05-10 14:03:32 -0400

    dd

---
+++ UnityGGJ2020/Assets/Scripts/HintItem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "hintObjects")]
+
+public class HintItem : ScriptableObject
+{
+    public Sprite hintSprite;
+    public Hints hints;
+    [SerializeField] private string description;
+}
