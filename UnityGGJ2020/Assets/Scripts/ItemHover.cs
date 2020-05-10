73344374ea290e14282942bc57fcabfb2d35e054
StevenPC <koreancoffe@gmail.com>
2020-05-10 02:53:10 -0400

    Scoopdiscoop WassLickityWoop

--- UnityGGJ2020/Assets/Scripts/ItemHover.cs
+++ UnityGGJ2020/Assets/Scripts/ItemHover.cs
@@ -6,17 +6,25 @@ public class ItemHover : MonoBehaviour
 {
     [SerializeField] private HintItem hint = null;
     [SerializeField] private InventorySystem invSys = null;
+    //Ray raycast;
+    //RaycastHit contact;
+    public Renderer renderer;
      
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<Renderer>();  
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("Hello World");
+    }
+
+
+
 }
