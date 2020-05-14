5d2fbc6545a5b4f3f16eeff2bdeabcf6373e2576
mr-fufu <monkeymanp8@gmail.com>
2020-05-13 21:53:36 -0400

    Add Additional Start Scene floating effects

--- UnityGGJ2020/Assets/Scripts/StartSceneParallax.cs
+++ UnityGGJ2020/Assets/Scripts/StartSceneParallax.cs
@@ -9,6 +9,9 @@ public class StartSceneParallax : MonoBehaviour
     private Vector2 transformHold;
     public GameObject allObjects;
 
+    private int xoffset = 300;
+    private int yoffset = 250;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +22,34 @@ public class StartSceneParallax : MonoBehaviour
     void Update()
     {
         mousePos = Input.mousePosition;
-        mousePos.x -= 460;
-        mousePos.y -= 310 + Mathf.PingPong(Time.time * 200, 500);
-        for(int i = 0; i < layer.Length; i++)
+        mousePos.x -= xoffset;
+        mousePos.y -= yoffset;
+
+        if (mousePos.x > xoffset)
+        {
+            mousePos.x = xoffset;
+        }
+        else if (mousePos.x < -xoffset)
+        {
+            mousePos.x = -xoffset;
+        }
+
+        if (mousePos.y > yoffset)
+        {
+            mousePos.y = yoffset;
+        }
+        else if (mousePos.y < -yoffset)
+        {
+            mousePos.y = -yoffset;
+        }
+
+        mousePos.y += Mathf.PingPong(Time.time * 150, 300);
+
+        for (int i = 0; i < layer.Length; i++)
         {
             transformHold = layer[i].transform.position;
-            transformHold.x = Mathf.Lerp(transformHold.x, mousePos.x * i * 0.01f, 0.005f);
-            transformHold.y = Mathf.Lerp(transformHold.y, mousePos.y * 0.5f * i * 0.01f, 0.005f);
+            transformHold.x = Mathf.Lerp(transformHold.x, mousePos.x * Mathf.Pow(i, 0.5f) * 0.05f, 0.004f);
+            transformHold.y = Mathf.Lerp(transformHold.y, mousePos.y * 0.8f * Mathf.Pow(i, 0.5f) * 0.05f, 0.004f);
             layer[i].transform.position = transformHold;
         }
     }
