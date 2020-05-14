9416751fe28628e3c47e62ad9e4f1923cf92cdde
mr-fufu <monkeymanp8@gmail.com>
2020-05-14 04:33:17 -0400

    Add More Float

---
+++ UnityGGJ2020/Assets/floatScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatScript : MonoBehaviour
+{
+    public GameObject sub;
+    public GameObject light1;
+    public GameObject light2;
+    private float floatOffset = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sub.transform.localPosition = new Vector2(sub.transform.localPosition.x, Mathf.PingPong(Time.time, 10) - 10);
+
+        light1.transform.localRotation = Quaternion.Euler(light1.transform.rotation.x, light1.transform.rotation.y, Mathf.PingPong(Time.time * 0.5f, 8) - 250);
+        light2.transform.localRotation = Quaternion.Euler(light2.transform.rotation.x, light2.transform.rotation.y, Mathf.PingPong(Time.time * 0.5f, 8) - 265);
+    }
+}
