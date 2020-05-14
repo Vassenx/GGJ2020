44b69e3ac8c25c00522507c00bb47d9eb98d62e9
StevenPC <koreancoffe@gmail.com>
2020-05-14 16:22:18 -0400

    scoop di scoop pikity whoop coop coop dun tun tun tun tun
    ItemHovering detection

--- UnityGGJ2020/Assets/Scripts/ItemHover.cs
+++ UnityGGJ2020/Assets/Scripts/ItemHover.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ItemHover : MonoBehaviour
 {
     [SerializeField] private HintItem hint = null;
     [SerializeField] private InventorySystem invSys = null;
+    [SerializeField] private TextMeshProUGUI txtmsh = null;
     //Ray raycast;
     //RaycastHit contact;
     public Renderer renderer;
