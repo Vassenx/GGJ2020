e497ad676370f0493bc457cfa2b0bdfda4ef428a
StevenPC <koreancoffe@gmail.com>
2020-07-14 21:00:52 -0400

    proper hinting

--- UnityGGJ2020/Assets/Scripts/UI/HintShow.cs
+++ UnityGGJ2020/Assets/Scripts/UI/HintShow.cs
@@ -22,26 +22,32 @@ public class HintShow : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     // Update is called once per frame
     void Update()
     {
-        
+        if (isShow)
+        {
+            temp = targetItem.transform;
+            Transform showDescriptionPosition = hintShowLocation.transform;
+            var tempPos = new Vector3(showDescriptionPosition.position.x,
+                                                         showDescriptionPosition.position.y,
+                                                         showDescriptionPosition.position.z);
+
+            targetItem.transform.position = Vector3.Lerp(targetItem.transform.position, tempPos, 0.2f);
+
+        }
+        else
+        {
+            targetItem.transform.position = hintHiddenLocation.transform.position;
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {      
         Debug.Log("I am hovering");
         isShow = true;
-        temp = targetItem.transform;
-        Transform showDescriptionPosition = hintShowLocation.transform;
-        var tempPos = new Vector3(showDescriptionPosition.position.x,
-                                                     showDescriptionPosition.position.y,
-                                                     showDescriptionPosition.position.z);
-
-        targetItem.transform.position = Vector3.Lerp(targetItem.transform.position, tempPos, 0.2f);
-
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        targetItem.transform.position = hintHiddenLocation.transform.position;
+        isShow = false;
     }
 
    
